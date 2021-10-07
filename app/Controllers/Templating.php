4ac08cb2a6be986008350020c35e364e57ae4739
DinaPutriAulia <nazaria20162016@gmail.com>
2021-10-07 14:26:18 +0700

    commit 4

--- app/Controllers/Templating.php
+++ app/Controllers/Templating.php
@@ -3,9 +3,13 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\UserModel;
 
 class Templating extends BaseController
 {
+	public function __construct(){
+		$this->userModel = new UserModel();
+	}
 	public function index()
 	{
 		$data = [
@@ -17,5 +21,29 @@ class Templating extends BaseController
 		// echo view('layouts/footer');
         return view('view_admin');
 
+	}
+
+	public function register()
+	{
+		$data = [
+			'title'=> "Blog - Posts"
+		];	
+		// echo view('layouts/header', $data);
+		// echo view('layouts/navbar');
+		// echo view('v_posts');
+		// echo view('layouts/footer');
+		return view('view_register');
+	}
+
+	public function saveRegister(){
+		$request = service('request');
+		$data =[
+			'fullname' => $request->getVar('fullname'),
+			'email' => $request->getVar('email'),
+			'password' => $request->getVar('password'),
+
+		];
+		$this->userModel->insert($data);
+		return redirect()->to('/register');
 	}
 }
